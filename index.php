4df897139b8cd7ad0ba644529f8e29338bd0ed29
Eduardo Diniz <dinizdudi@gmail.com>
2018-06-07 13:58:54 -0300

    criado a tela de login e autenticação

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <?php 
 
+SESSION_START();
+
 require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
 use \Hcode\PageAdmin;
-
+use \Hcode\Model\User;
 
 $app = new Slim();
 
@@ -21,9 +23,40 @@ $app->get('/', function() {
 
 $app->get('/admin', function() {
     
+	User::verifyLogin();
+
 	$page = new PageAdmin(); //usa a classe PageAdmin, que faz o construct colocar o header
 
 	$page->setTpl("index"); //qual html será usado na pagina || depois dessa linha chama o destruct que coloca o footer
+
+});
+
+$app->get("/admin/login", function(){
+
+	$page = new PageAdmin([
+		"header" => false,
+		"footer" => false
+	]);
+
+	$page->setTpl("login");
+
+});
+
+$app->post("/admin/login", function(){
+
+	User::login($_POST["login"], $_POST["password"]);
+
+	header("location: /admin");
+	exit;
+
+});
+
+$app->get("/admin/logout", function(){
+
+	User::logout();
+
+	header("location: /admin/login");
+	exit;
 
 });
 
---
+++ vendor/hcodebr/php-classes/src/Model.php
@@ -0,0 +1,46 @@
+<?php 
+
+namespace Hcode;
+
+class Model {
+
+	private $values = [];
+
+	public function __call($name, $args)
+	{
+
+		$method = substr($name, 0 , 3);
+		$fieldName = substr($name, 3, strlen($name));
+
+		switch($method)
+		{
+			case "get":
+				return $this->values[$filedName]; 
+				// get -> retorna o valor do campo em questão.Ficaria tipo "return $this->values[iduser]" que vai retornar o valor do id do usuario que está logando
+			break;
+
+			case "set":
+				$this->values[$fieldName] = $args[0];
+				// set -> atribui o valor do campo em questão.Ficaria tipo "$this->values[iduser] = 1" já que o ARGS é o valor do campo que foi mandado pro __call
+			break;
+		}
+
+	}
+
+	public function setData($data = array())
+	{
+		foreach ($data as $key => $value) { //data são todos os campos que retornou, key é o nome do campo, e value é o valor do campo
+			
+			$this->{"set".$key}($value);
+
+		}
+	}
+
+	public function getValues()
+	{
+		return $this->values;
+	}
+
+}
+
+ ?>
